3c8f13cdfb9c5b5b3b82a749fd48dd3e5792091a
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2025-10-20 23:12:58 +0530

    add new router getUserChannelProfileById

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,7 +10,8 @@ import {
     updateUserCoverImage, 
     getUserChannelProfile, 
     getWatchHistory, 
-    updateAccountDetails
+    updateAccountDetails,
+    getUserChannelProfileById
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -46,5 +47,7 @@ router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updat
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
+router.route("/id/:userId").get(verifyJWT, getUserChannelProfileById)
+
 
 export default router
